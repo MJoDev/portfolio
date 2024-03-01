8537ee73e28a940e978ea8921681230cca960d77
Moisés David Jiménez Ortiz <143739485+MJoDev@users.noreply.github.com>
2024-03-01 10:33:01 -0400

    Skills

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,17 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faAngular } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faNodeJs } from '@fortawesome/free-brands-svg-icons';
+import { faDocker } from '@fortawesome/free-brands-svg-icons';
+import { faPython } from '@fortawesome/free-brands-svg-icons';
+import { faJava } from '@fortawesome/free-brands-svg-icons';
+import { faCss3 } from '@fortawesome/free-brands-svg-icons';
+import { faHtml5 } from '@fortawesome/free-brands-svg-icons';
+import { faBootstrap } from '@fortawesome/free-brands-svg-icons';
+import { faGit } from '@fortawesome/free-brands-svg-icons';
+import { faJenkins } from '@fortawesome/free-brands-svg-icons';
+
+
 
 
 @Component({
@@ -15,8 +26,21 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 export class HomeComponent implements OnInit {
   
   faAngular = faAngular;
+  faNodeJs = faNodeJs;
+  faDocker = faDocker;
+  faPython = faPython;
+  faJava = faJava;
+  faCss3 = faCss3;
+  faHtml5 = faHtml5;
+  faBootstrap = faBootstrap;
+  faGit = faGit;
+  faJenkins = faJenkins;
+
+  
   faLinkedin = faLinkedin;
   faGithub = faGithub;
+
+
 
   ngOnInit(): void {
       
